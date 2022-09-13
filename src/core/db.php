4ca17770228bcaf2db0e5dd27dff9659f9c989e6
Grzegorz Miśkiewicz <biuro@avatec.pl>
2022-09-13 20:11:56 +0200

    no message

---
+++ src/core/db.php
@@ -0,0 +1,337 @@
+<?php
+namespace Core;
+
+use mysqli;
+use Exception;
+/**
+ *	Klasa obsługuje połączenie z MySQL w systemie CMS
+ *  @author Grzegorz Miśkiewicz <biuro@avatec.pl>
+ *  @version 2.0
+ *  @copyright Avatec.pl
+ */
+
+class Db
+{
+    private static $instance;
+    public static $debug;
+
+    private function __clone()
+    {
+    }
+
+    public function __destruct()
+    {
+        self::$instance->close();
+    }
+
+    public static function call()
+    {
+        global $config;
+
+        if (!isset(self::$instance)) {
+            self::$instance = new MySQLi($config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name'], (!empty($config['db_port']) ? $config['db_port'] : 3306));
+
+            if (self::$instance->connect_error) {
+                self::$instance = new MySQLi($config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name'], (int) $config['db_port']);
+
+                if (self::$instance->connect_error) {
+                    throw new Exception('Error MySQL: ' . self::$instance->connect_error);
+                }
+            }
+
+            self::$instance->set_charset("utf8mb4");
+            self::$instance->query("SET sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';");
+            self::$instance->query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+        }
+
+        if (isset(self::$instance)) {
+            return self::$instance;
+        }
+    }
+
+    public static function error()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        return self::$instance->error;
+    }
+
+    public static function real_escape($string)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        return self::$instance->real_escape_string($string);
+    }
+
+    public static function count($t, $w = null)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+        if (is_null($w)) {
+            $result = self::$instance->query("SELECT * FROM " . $t);
+        } else {
+            $result = self::$instance->query("SELECT * FROM " . $t . " WHERE " . $w);
+        }
+
+        if (!empty($result)) {
+            return $result->num_rows;
+        } else {
+            return 0;
+        }
+    }
+
+    public static function query($query)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        if (self::$debug == true) {
+            echo $query . PHP_EOL;
+        }
+
+        $result = self::$instance->query($query);
+        if (!empty(self::$instance->error)) {
+            throw new Exception('Error MySQL: ' . self::$instance->error);
+        }
+
+        while ($row = $result->fetch_assoc()) {
+            $array[] = $row;
+        }
+
+        if (isset($array)) {
+            return $array;
+        }
+    }
+
+    public static function query_row($query)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        if (!empty(self::$debug)) {
+            self::showDebug($query, __CLASS__ . '::' . __FUNCTION__ . ' on line: ' . __LINE__);
+        }
+
+        $result = self::$instance->query($query);
+        if (!empty(self::$instance->error)) {
+            throw new Exception('Error MySQL: ' . self::$instance->error);
+        }
+
+        if (!empty($result)) {
+            while ($row = $result->fetch_assoc()) {
+                $array[] = $row;
+            }
+        }
+        if (isset($array[0])) {
+            return $array[0];
+        }
+    }
+
+    public static function last_id($table, $column = "id")
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        $result = self::row("*", $table, "ORDER BY ".$column." DESC");
+        return $result[$column];
+    }
+
+    public static function run($query)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        $result = self::$instance->query($query);
+        if (!empty(self::$instance->error)) {
+            throw new Exception('Error MySQL: ' . self::$instance->error);
+        }
+
+        if (!empty($result->num_rows)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function update($t, $q, $w)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        $query = "UPDATE " . $t . " SET " . $q . " WHERE ".$w;
+        if (self::$debug == true) {
+            throw new Exception("MySQL ERROR: " . $query);
+        }
+        if (self::$instance->query($query) === true) {
+            return true;
+        } else {
+            if (self::$debug == true) {
+                throw new Exception("MySQL ERROR: " . $query);
+                exit;
+            }
+            throw new Exception('Error MySQL: ' . $query . ' => ' . self::$instance->error);
+        }
+    }
+
+    public static function exec($e = "*", $t = null, $w = null)
+    {
+        if (self::$debug == true) {
+            echo "Function exec: <b>SELECT " . $e . " FROM " . $t . " " . $w . "</b><br/>";
+        }
+        $result = self::query("SELECT " . $e . " FROM " . $t . " " . $w);
+        return $result;
+    }
+
+    public static function row($e = "*", $t = null, $w = null)
+    {
+        $query = "SELECT " . $e . " FROM " . $t . " " . $w;
+
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        if (self::$debug == true) {
+            echo $query . PHP_EOL;
+            //exit;
+        }
+
+        $result = self::$instance->query($query);
+        if (!empty(self::$instance->error)) {
+            throw new Exception('Error MySQL: ' . self::$instance->error);
+        }
+
+        $result = $result->fetch_assoc();
+        return $result;
+    }
+
+    public static function check($t, $w, $silent = false)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+        $query = "SELECT * FROM " . $t . " WHERE " . $w;
+
+        if (self::$debug == true) {
+            echo "Function check: <b>".$query."</b><br/>";
+        }
+
+        $result = mysqli_query(self::$instance, $query);
+        if (!empty($result) && $result->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function insert($t, $v)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        $query = "INSERT INTO " . $t . " VALUES(" . $v . ");";
+
+        if (self::$debug == true) {
+            echo "Function insert: <b>".$query."</b><br/>";
+        }
+
+        $result = mysqli_query(self::$instance, $query);
+        return $result;
+    }
+
+    public static function insert_id()
+    {
+        if (!empty(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        $id = self::$instance->insert_id;
+        return $id;
+    }
+
+    public static function save($t, $v, $w)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        $query = "UPDATE  " . $t . " SET " . $v . " WHERE " . $w . ";";
+
+        if (self::$debug == true) {
+            echo "Function save: <b>".$query."</b><br/>";
+        }
+
+        $result = mysqli_query(self::$instance, $query);
+        return $result;
+    }
+
+    public static function delete($t, $w)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        $query = "DELETE FROM  " . $t . " WHERE " . $w . ";";
+
+        if (self::$debug == true) {
+            echo "Function delete: <b>".$query."</b><br/>";
+        }
+
+        $result = mysqli_query(self::$instance, $query);
+
+        $query = "OPTIMIZE TABLE " . $t;
+        mysqli_query(self::$instance, $query);
+
+        return $result;
+    }
+
+    public static function has_table($t)
+    {
+        global $config;
+
+        if (!isset(self::$instance)) {
+            self::$instance = self::call();
+        }
+
+        $result = self::$instance->query("SHOW TABLES FROM " . $config['db_name'] . " LIKE '" . $t . "';");
+        if (!empty(self::$instance->error)) {
+            throw new Exception('Error MySQL: ' . self::$instance->error);
+        }
+
+        if (!empty($result->num_rows)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function install($sql_array)
+    {
+        if ((!empty($sql_array)) && (is_array($sql_array))) {
+            foreach ($sql_array as $sql) {
+                self::run($sql);
+            }
+        } else {
+            if (!empty($sql_array)) {
+                self::run($sql_array);
+            }
+        }
+
+        return true;
+    }
+
+    public static function close()
+    {
+        self::$instance->close();
+    }
+}
---
+++ src/core/route.php
@@ -0,0 +1,162 @@
+<?php
+namespace Core;
+
+use Core\Language;
+
+class Route
+{
+    public $num = 0;
+    public $path = '/';
+    public $path_array = [];
+    public $paths = [];
+    public $results = array();
+
+    public $error_code = null;
+
+    // Jeżeli ścieżka zawiera /admin/
+    public $isAdmin 	= false;
+
+    // Jeżeli jest to główna strona
+    public $isMain		= false;
+    public $isApi       = false;
+    public $isCron 		= false;
+    public $isError 	= false;
+    public $isUpload 	= false;
+    public $isOAuth2 	= false;
+
+    public $isLanguage 	= true;
+    public $language 	= 'pl';
+    public $asCatalog	= false;
+    public $catalog 	= '/';
+
+    protected $replace_pattern = [
+        ':id' => '([\d]+)',
+        ':string' => '([^/]+)',
+        ':file' => '([\w\s-]+\.[A-Za-z]+)',
+        ':any' => '(.+)'
+    ];
+
+    public function __construct($catalog = null)
+    {
+        if (!is_null($catalog)) {
+            $this->catalog = $catalog;
+        }
+        $this->get_path();
+    }
+
+    public function get_path()
+    {
+        $this->path = $_SERVER['REQUEST_URI'];
+        $this->path = str_replace("?" . $_SERVER['QUERY_STRING'], "", $this->path);
+        $this->path_array = array_filter(explode("/", $this->path));
+        $this->path_array = array_values($this->path_array);
+
+        if (!empty($this->path_array[0]) && in_array($this->path_array[0], array_keys(Language::$available)) == true) {
+            $this->language = $this->path_array[0];
+            Language::change($this->language);
+            unset($this->path_array[0]);
+            $this->path_array = array_values($this->path_array);
+            $this->path = implode("/", $this->path_array);
+        }
+
+        if (empty($this->path_array[0])) {
+            $check_path = '/';
+        } else {
+            if ($this->path_array[0] == $this->catalog) {
+                $check_path = (!empty($this->path_array[1]) ? $this->path_array[1] : '/');
+            } else {
+                $check_path = $this->path_array[0];
+            }
+        }
+
+        $this->num = count($this->path_array);
+
+        switch ($check_path) {
+            case "/":
+                $this->isMain = true;
+            break;
+
+            case "admin":
+                $this->isAdmin = true;
+            break;
+
+            case "cron":
+                $this->isCron = true;
+            break;
+
+            case "api":
+                $this->isApi = true;
+            break;
+
+            case "upload":
+                $this->isUpload = true;
+            break;
+
+            case "oauth":
+                $this->isOAuth2 = true;
+            break;
+
+            case "error":
+                $this->isError = true;
+                $this->error_code = end($this->path_array);
+            break;
+        }
+    }
+
+    protected function parse($regexp)
+    {
+        return str_replace(array_keys($this->replace_pattern), array_values($this->replace_pattern), $regexp);
+    }
+
+    public function get($regexp, $option)
+    {
+        if (in_array($regexp, $this->paths) !== false) {
+            //Kernel::log('routing.log', $this->path . ' has been defined before ' . __FILE__);
+            return;
+        } else {
+            $this->paths[] = $regexp;
+        }
+
+        preg_match('#^/?' . $this->parse($regexp) . '/?$#', $this->path, $m);
+        if (empty($m[0])) {
+            return;
+        }
+
+        if (is_callable($option)) {
+            $this->results = call_user_func($option);
+        } else {
+            $this->results = $option;
+        }
+
+        if (!empty($this->results['id'])) {
+            if (!is_array($this->results['id'])) {
+                if (strpos($this->results['id'], '$') !== false) {
+                    $this->results['id'] = $m[str_replace('$', '', $this->results['id'])];
+                }
+            } else {
+                $id = [];
+                foreach ($this->results['id'] as $k=>$i) {
+                    $this->results['id'][$k] = $m[str_replace('$', '', $i)];
+                }
+            }
+        }
+
+        if (!empty($this->results['command']) && strpos($this->results['command'], '$') !== false) {
+            $this->results['command'] = $m[str_replace('$', '', $this->results['command'])];
+        }
+
+        if (empty($this->results['file']) && !empty($this->results['module'])) {
+            $this->results['file'] = $this->results['module'];
+        }
+
+        global $app_path;
+
+        if (!empty($this->results['module'])) {
+            if (file_exists($app_path . 'modules/' . $this->results['module'] . '/frontend/' . $this->results['file'] . '.frontend.php') == true) {
+                $this->results['url'] = $app_path . 'modules/' . $this->results['module'] . '/frontend/' . $this->results['file'] . '.frontend.php';
+            } else {
+                $this->results['url'] = $app_path . 'modules/' . $this->results['module'] . '/' . $this->results['file'] . '.website.php';
+            }
+        }
+    }
+}
