c58e6c316b06b33ffcd346a892a5137635071b52
Grzegorz Miśkiewicz <biuro@avatec.pl>
2022-10-20 12:44:11 +0200

    no message

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0afd9190047c6a9b6341a94daa5a2568a31937cd',
+        'name' => 'avatec/framework-core',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'avatec/framework-core' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0afd9190047c6a9b6341a94daa5a2568a31937cd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
