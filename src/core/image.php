ca06110e57541cffec4582d37dfb0e80b9ed5a71
Grzegorz Miśkiewicz <biuro@avatec.pl>
2022-11-07 13:52:07 +0100

    no message

--- src/core/image.php
+++ src/core/image.php
@@ -316,28 +316,26 @@ class Image
 		return 'unknown / ' . $result;
 	}
 
-	public function colorThief($img, $default = '#eee')
+	public function getDominantColor($img, $default = '#eee')
 	{
-		if (@exif_imagetype($img)) { // CHECK IF IT IS AN IMAGE
-			$type = getimagesize($img)[2]; // GET TYPE
-			if ($type === 1) { // GIF
+		if (@exif_imagetype($img)) { 
+			$type = getimagesize($img)[2]; 
+			if ($type === 1) { 
 				$image = imagecreatefromgif($img);
-				// IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
 				if (imagecolorsforindex($image, imagecolorstotal($image) - 1)['alpha'] == 127) return 'fff';
 			} else if ($type === 2) { // JPG
 				$image = imagecreatefromjpeg($img);
 			} else if ($type === 3) { // PNG
 				$image = imagecreatefrompng($img);
-				// IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
 				if ((imagecolorat($image, 0, 0) >> 24) & 0x7F === 127) return 'fff';
-			} else { // NO CORRECT IMAGE TYPE (GIF, JPG or PNG)
+			} else {
 				return $default;
 			}
-		} else { // NOT AN IMAGE
+		} else {
 			return $default;
 		}
-		$newImg = imagecreatetruecolor(1, 1); // FIND DOMINANT COLOR
+		$newImg = imagecreatetruecolor(1, 1); 
 		imagecopyresampled($newImg, $image, 0, 0, 0, 0, 1, 1, imagesx($image), imagesy($image));
-		return dechex(imagecolorat($newImg, 0, 0)); // RETURN HEX COLOR
+		return dechex(imagecolorat($newImg, 0, 0)); 
 	}
 }
