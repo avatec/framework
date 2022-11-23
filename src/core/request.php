7a0a2b648e71b6d26920a95d7b1c863a93f71490
Grzegorz Miśkiewicz <biuro@avatec.pl>
2022-11-23 10:31:54 +0100

    no message

--- src/core/request.php
+++ src/core/request.php
@@ -40,7 +40,7 @@ class Request
         
         if (is_array($data)) {
             foreach ($data as $key => $value) {
-                if(!empty( $data[$key] )) {
+                if(!empty( $data[$key] ) && is_array( $data[$key] )) {
                     $data[$this->clean($key)] = $this->clean($value);
                 }
             }
