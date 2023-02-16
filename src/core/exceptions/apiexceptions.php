e5c3fc77c97e13dd28ee75bfdfe47eef8b6fa968
Grzegorz Miśkiewicz <biuro@avatec.pl>
2023-02-16 13:24:44 +0100

    no message

--- src/core/exceptions/apiexceptions.php
+++ src/core/exceptions/apiexceptions.php
@@ -1,4 +1,6 @@
-<?php namespace Core\Exceptions;
+<?php
+
+namespace Core\Exceptions;
 
 use Exception;
 
@@ -9,22 +11,16 @@ class ApiExceptions extends Exception
     protected $message;
     protected $data;
 
-    public function __construct($message = "", $code = 0, $data = null) {
+    public function __construct($message = "", $code = 0, $data = null)
+    {
         $this->message = $message;
         $this->code = $code;
         $this->data = $data;
         parent::__construct($message, $code);
     }
 
-    public function getCode() {
-        return $this->code;
-    }
-
-    public function getMessage() {
-        return $this->message;
-    }
-
-    public function getData() {
+    public function getData()
+    {
         return $this->data;
     }
 }
