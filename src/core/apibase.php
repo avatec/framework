66a72d5b41fd28d75769c0c9525ed45d73e7196b
Grzegorz Miśkiewicz <biuro@avatec.pl>
2023-02-16 16:04:38 +0100

    no message

--- src/core/apibase.php
+++ src/core/apibase.php
@@ -17,7 +17,6 @@ class ApiBase
 
     public function validateAuth()
     {
-        print_r($_SERVER);
         if( $this->authToken !== $this->siteAuthToken ) {
             throw new ApiExceptions('Invalid auth token provided' , 401);
         }
