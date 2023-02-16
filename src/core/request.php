fe170c4e460fd1ba7ba1f1911986ddd8ad473e70
Grzegorz Miśkiewicz <biuro@avatec.pl>
2023-02-16 15:49:20 +0100

    no message

--- src/core/request.php
+++ src/core/request.php
@@ -116,7 +116,7 @@ class Request
 
         $headers = [
             'Content-Type: multipart/form-data',
-            'SiteAuth: ' . $authToken
+            'Auth: ' . $authToken
         ];
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
