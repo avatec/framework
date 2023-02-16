5db8ed8385d8571ed672eb6cacf66379629a8ad2
Grzegorz Miśkiewicz <biuro@avatec.pl>
2023-02-16 13:42:56 +0100

    no message

---
+++ src/core/apibase.php
@@ -0,0 +1,24 @@
+<?php namespace Core;
+
+use Core\Exceptions\ApiExceptions;
+
+class ApiBase 
+{
+    private $authToken;
+    private $siteAuthToken;
+
+    public function __construct( string $authToken )
+    {
+        $this->authToken = $authToken;
+        if (isset($_SERVER['HTTP_SITEAUTH'])) {
+            $this->siteAuthToken = $_SERVER['HTTP_SITEAUTH'];
+        }
+    }
+
+    public function validateAuth()
+    {
+        if( $this->authToken !== $this->siteAuthToken ) {
+            throw new ApiExceptions('Invalid auth token provided' , 401);
+        }
+    }
+}
