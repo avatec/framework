5f7dc9e5afeb1ad16429b3a4d783421e3e740d66
Grzegorz Miśkiewicz <biuro@avatec.pl>
2023-02-16 16:29:00 +0100

    no message

--- src/core/request.php
+++ src/core/request.php
@@ -113,7 +113,11 @@ class Request
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        if( $method == 'POST' ) {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        } elseif( $method == 'GET' ) {
+            curl_setopt($ch, CURLOPT_HTTPGET, true);
+        }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $headers = [
