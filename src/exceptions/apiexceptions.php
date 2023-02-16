2028c07c5bb0202c327d5a63a0baa10eba728f1b
Grzegorz Miśkiewicz <biuro@avatec.pl>
2023-02-16 13:18:08 +0100

    no message

---
+++ src/exceptions/apiexceptions.php
@@ -0,0 +1,28 @@
+<?php namespace Exceptions;
+
+class ApiExceptions extends Exception
+{
+    // Exception code, message and data properties
+    private $code;
+    private $message;
+    private $data;
+
+    public function __construct($message = "", $code = 0, $data = null) {
+        $this->message = $message;
+        $this->code = $code;
+        $this->data = $data;
+        parent::__construct($message, $code);
+    }
+
+    public function getCode() {
+        return $this->code;
+    }
+
+    public function getMessage() {
+        return $this->message;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+}
