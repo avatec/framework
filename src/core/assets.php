ca449b86d26882c37dcec10eb160a1f9aa37506e
Grzegorz Miśkiewicz <biuro@avatec.pl>
2023-02-27 17:09:27 +0100

    no message

--- src/core/assets.php
+++ src/core/assets.php
@@ -16,7 +16,7 @@ class Assets
         }
 
         $template = self::$frontend ? 'website' : 'admin';
-        $modulePath = is_null($module) ? '' : '/modules/' . $module . '/' . (self::$frontend ? 'frontend' : 'backend');
+        $modulePath = is_null($module) ? '' : 'modules/' . $module . '/' . (self::$frontend ? 'frontend' : 'backend');
 
         return "/templates/$template/$modulePath/$type/";
     }
