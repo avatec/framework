8ef5a31a705b2fa0752590486b49f75e0c6e8d1b
Grzegorz Miśkiewicz <biuro@avatec.pl>
2023-03-11 16:34:42 +0100

    no message

---
+++ src/core/sms.php
@@ -0,0 +1,30 @@
+<?php namespace Core;
+
+use SMSApi\Client;
+use SMSApi\Api\Sms\Message\Send;
+use SMSApi\Exception\SmsapiException;
+
+class SMS {
+
+    private $client;
+  
+    public function __construct($apiKey) 
+    {
+        $this->client = new Client($apiKey);
+    }
+  
+    public function sendMessage($to, $message) 
+    {
+        $sms = new Send();
+        $sms->setTo($to);
+        $sms->setText($message);
+  
+        try {
+            $result = $this->client->smsSend()->send($sms); 
+            return $result->getList()[0]->getPoints();
+        } catch (SmsapiException $e) {
+            throw new \Exception("Error sending SMS: " . $e->getMessage());
+            return false;
+        }
+    }
+}
